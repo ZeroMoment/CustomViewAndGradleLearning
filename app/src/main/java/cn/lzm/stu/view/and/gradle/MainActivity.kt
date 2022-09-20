b5a0ae7089092ce13fe0b3008d69f5efe9c58bd3
lizhiming <zhiming.li@wm-motor.com>
2022-09-20 10:34:16 +0800

    first commit

---
+++ app/src/main/java/cn/lzm/stu/view/and/gradle/MainActivity.kt
@@ -0,0 +1,11 @@
+package cn.lzm.stu.view.and.gradle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
